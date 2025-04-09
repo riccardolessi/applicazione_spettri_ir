7f4244b49689aeb02e7d434a1c7c4b426cbbc3b9
riccardolessi <lessi.riccardo@libero.it>
2025-04-09 16:24:07 +0200

    finito lavoro per import db con interfaccia

--- lib/test2.py
+++ lib/test2.py
@@ -6,32 +6,75 @@ import pandas as pd
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 db_path = os.path.join(base_dir, "spettri.db")
 
-# Query al DB per vedere tabelle e colonne
-def tabelle():
-    # Connessione al database SQLite
+# Query al DB per vedere i gruppi funzionali
+def gruppi_funzionali():
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-
-    # 1. Ottenere tutte le tabelle
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    tabelle = cursor.fetchall()
-
-    # Chiudere la connessione
+    cursor.execute("SELECT * FROM gruppi_funzionali")
+    row = cursor.fetchall()
     conn.close()
 
-    tabelle = [x for x in tabelle if x != 'sqlite_sequence' and x != 'spettri_old']
+    output = {banda[0]: banda[1] for banda in row}
 
-    tabelle_new = [x[0] for x in tabelle]
+    return output
 
-    # Restituire il risultato come stringa
-    return tabelle_new
-
-def colonne(nome_tabella):
+def fonti():
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
+    cursor.execute("SELECT * FROM fonti")
+    row = cursor.fetchall()
+    conn.close()
 
-    cursor.execute(f"PRAGMA table_info({nome_tabella});")
+    output = {fonte[0]: fonte[1] for fonte in row}
 
-    colonne = cursor.fetchall()
+    return output
 
-    return colonne
+def salva_fonte(nome_fonte):
+
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO fonti (nome) VALUES(?)", (nome_fonte, ))
+        
+        conn.commit()
+        conn.close()
+
+        return "success"
+
+    except sqlite3.Error as e:
+        return f"Errore: {e}"
+    
+def salva_gruppo_funzionale(nome_gruppo, smiles):
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        if smiles != "":
+            cursor.execute("INSERT INTO gruppi_funzionali (gruppo_funzionale, smile_gruppo) VALUES (?, ?)", (nome_gruppo, smiles))
+        else:
+            cursor.execute("INSERT INTO gruppi_funzionali (gruppo_funzionale) VALUES (?)", (nome_gruppo, ))
+
+        conn.commit()
+        conn.close()
+
+        return "success"
+
+    except sqlite3.Error as e:
+        return f"Errore: {e}"
+    
+def salva_banda_gruppo_funzionale(nome_banda, min, max, id_gruppo_funzionale, fonte_banda):
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO bande_gruppi_funzionali
+            (gruppo_funzionale, min, max, id_gruppo, fonte_bande)
+            VALUES (?, ?, ?, ?, ?)
+        """, (nome_banda, min, max, id_gruppo_funzionale, fonte_banda))
+
+        conn.commit()
+        conn.close()
+
+        return "success"
+
+    except sqlite3.Error as e:
+        return f"Errore: {e}"
--- modules/query_2.py
+++ modules/query_2.py
@@ -1,46 +1,146 @@
 from shiny import module, ui, reactive, render
 import pandas as pd
 
+# Funzione per mostrare notifiche
+def mostra_notifica(messaggio, tipo_notifica = "message", durata = 4):
+    ui.notification_show(
+        messaggio,
+        type = tipo_notifica,
+        duration= durata
+    )
+
+
 @module.ui
 def insert_db_ui():
     return (
-        ui.h2("Prova Data Frame editabile"),
-        ui.input_select(
-            "select",
-            "Seleziona tabella",
-            choices = []
-        ),
-        ui.output_data_frame("prova"),
-        ui.output_text("show_data"), 
-        ui.input_action_button("bottone_test", "Clicca qui"),
-        ui.input_action_button("bottone_due", "Salva")
+        ui.navset_pill_list(  
+            ui.nav_panel(
+                "Gruppi Funzionali",
+                ui.input_text("gruppo_funzionale", "Nome gruppo funzionale"),
+                ui.input_text("smile_gruppo", "SMILES del gruppo funzionale"),
+                ui.input_action_button("save_gf", "Salva")
+            ),
+            ui.nav_panel(
+                "Bande Gruppi Funzionali",
+                ui.input_text("bange_gruppo_funzionale", "Nome Banda"),
+                ui.input_numeric("min_banda", "Minimo banda", 1, min = 1, max = 4000),
+                ui.input_numeric("max_banda", "Massimo banda", 1, min = 1, max = 4000),
+                ui.input_select("lista_gruppi_funzionali", "Seleziona il gruppo funzionale", choices = []),
+                ui.input_select("lista_fonte_bande", "Seleziona la fonte della banda", choices = []),
+                ui.input_action_button("save_bande_gf", "Salva")
+            ),
+            ui.nav_panel(
+                "Fonti",
+                ui.input_text("nome_fonte", "Nome Fonte"),
+                ui.input_action_button("save_fonti", "Salva")
+            )   
+        )
     )
 
 @module.server
 def insert_db_server(input, output, session, test):
-    
+    #-------------------
+    # Logica fonti
+    #-------------------
+
     @reactive.effect
-    def select():
-        tabelle = test.tabelle()
-        ui.update_select(
-            "select",
-            choices = tabelle
-        )
-    
-    @reactive.effect
-    @reactive.event(input.bottone_test)
+    @reactive.event(input.save_fonti)
     def _():
-        colonne = test.colonne(input.select())
-        lista_colonne = [x[1] for x in colonne]
-        print(lista_colonne)
-        
-        df = pd.DataFrame(columns = lista_colonne)
-        pippo = ['' for _ in range(len(lista_colonne))]
-        print(pippo)
-        df.loc[0] = pippo
+        if input.nome_fonte():
+            print(f"salvato in fonti")
+            print(f"Nome fonte: {input.nome_fonte()}")
+            
+            result = test.salva_fonte(input.nome_fonte())
+            
+            if result == "success":
+                mostra_notifica(
+                    "Record salvato con successo le DB"
+                )
+            else :
+                mostra_notifica(
+                    result,
+                    tipo_notifica = "error"
+                )
+        else:
+            mostra_notifica(
+                "E' necessario compilare tutti i campi",
+                tipo_notifica = "error"
+            )
 
-        print(df)
 
-        @render.data_frame
-        def prova():
-            return render.DataGrid(df, editable = True)
+    #-------------------
+    # Logica bande gruppi funzionali
+    #-------------------
+    @reactive.effect
+    def _():
+        fonti = test.fonti()
+        ui.update_select("lista_fonte_bande", choices = fonti)
+
+    # Inserimento nel select dei gruppi funzionali
+    @reactive.effect
+    def _():
+        bande_gf = test.gruppi_funzionali()
+        ui.update_select("lista_gruppi_funzionali", choices = bande_gf)
+
+    # Logica per gestire il bottone per le bande dei gruppi funzionali
+    @reactive.effect
+    @reactive.event(input.save_bande_gf)
+    def _():
+        if input.bange_gruppo_funzionale() and input.min_banda() and input.max_banda() and input.lista_gruppi_funzionali():
+            if (input.max_banda() <= input.min_banda()):
+                mostra_notifica(
+                    "Il massimo della banda deve essere superiore al minimo",
+                    tipo_notifica = "error"
+                )
+                return None
+            
+            print(f"salvato in bande gruppi funzionali")
+            print(f"nome banda: {input.bange_gruppo_funzionale()}")
+            print(f"min: {input.min_banda()}")
+            print(f"max: {input.max_banda()}")
+            print(f"id gruppo funzionale: {input.lista_gruppi_funzionali()}")
+            print(f"fonte: {input.lista_fonte_bande()}")
+            
+            
+            # Logica del DB
+            result = test.salva_banda_gruppo_funzionale(
+                input.bange_gruppo_funzionale(),
+                input.min_banda(),
+                input.max_banda(),
+                input.lista_gruppi_funzionali(),
+                input.lista_fonte_bande()
+            )
+            
+            if result == "success":
+                mostra_notifica("Record salvato con successo nel database")
+            else :
+                mostra_notifica(result, tipo_notifica = "error")
+        else:
+            mostra_notifica(
+                "E' necessario compilare tutti i campi",
+                tipo_notifica = "error"
+            )
+
+    #-------------------
+    # Logica gruppi funzionali
+    #-------------------
+
+    # Logica per gestire il bottone per i gruppi funzionali
+    @reactive.effect
+    @reactive.event(input.save_gf)
+    def _():
+        if input.gruppo_funzionale():
+            print(f"salvato in gruppi funzionali")
+            print(f"{input.gruppo_funzionale()} {input.smile_gruppo()}")
+            
+            result = test.salva_gruppo_funzionale(input.gruppo_funzionale(), input.smile_gruppo())
+            
+            if result == "success":
+                mostra_notifica("Record salvato con successo nel database")
+            else :
+                mostra_notifica(result, tipo_notifica = "error")
+        else:
+            mostra_notifica(
+                "E' necessario compilare tutti i campi",
+                tipo_notifica = "error"
+            )
